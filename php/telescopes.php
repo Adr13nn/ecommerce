64bd9d185be6214e30d83e0f96bf519535113be4
Adr13nn <btr.adrien@gmail.com>
2020-11-30 12:37:51 +0100

    maj CSS

--- php/telescopes.php
+++ php/telescopes.php
@@ -9,7 +9,7 @@
     <h1><a href="index.php?page=vuearticle&art=<?= $index ?>"><?= $article["titre"] ?></a></h1>
     <p class="description"><?= substr($article["description"], 0, 100) ?>..<a href="index.php?page=vuearticle&art=<?= $index ?>"><br><strong>Lire la suite...</a></strong></p>
     <div class="images">
-    <div class=""><img class="" src= <?= $article["image"] ?> alt=""></div>
+    <div class="image"><img class="" src= <?= $article["image"] ?> alt=""></div>
     </div>
     <p class="fabricant">Fabricant: <?= $article["fabricant"] ?></p>
     <p class="stock">Disponibilité: <strong> <?= $article["stock"] ?></strong></p>
