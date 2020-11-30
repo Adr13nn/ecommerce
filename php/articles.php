57a004759468cfaadfbbffb44502f1a9bff96410
Adr13nn <btr.adrien@gmail.com>
2020-11-30 10:01:42 +0100

    correction erreur ligne 6

--- php/articles.php
+++ php/articles.php
@@ -2,7 +2,7 @@
 
 $articleT_1 = [
     "titre" => "Télescope Omegon AC 70/700 AZ-2",
-    "fabricant" => "Omegon"
+    "fabricant" => "Omegon",
     "description" => "La lunette astronomique Omegon AC 70/700 - votre porte d'entrée vers le monde des étoiles. Elle est tellement facile à utiliser qu'elle convient parfaitement aussi bien pour les enfants que les adultes. L'instrument est facile à comprendre et rapide à monter, sans outillage - tout à fait dans le style: Vous le posez, vous insérez l'oculaire et voilà - tout est prêt pour l'observation !",
     "image" => "images/t_1.webp",
     "prix" => "99 €",
@@ -12,7 +12,7 @@ $articleT_1 = [
 
 $articleT_2 = [
     "titre" => "Télescope Omegon N 150/750 EQ-3",
-    "fabricant" => "Omegon"
+    "fabricant" => "Omegon",
     "description" => "Image similaire. (Instrument fourni avec un chercheur à point lumineux au lieu d'une lunette de visée 6 x 30). La lunette de type Newton 150/750 EQ-3 est l'instrument idéal pour l'initiation à l'astronomie. Il se distingue par un bon système optique et une monture déjà très stable.",
     "image" => "images/t_2.webp",
     "prix" => "299 €",
@@ -22,7 +22,7 @@ $articleT_2 = [
 
 $articleT_3 = [
     "titre" => "Télescope Omegon N 114/900 EQ-1",
-    "fabricant" => "Omegon"
+    "fabricant" => "Omegon",
     "description" => "Le télescope classique de Newton, inventé par Isaac newton. Ses dimensions et ce type de construction sont présents sur le marché depuis trois siècles et ont fait leurs preuves. ",
     "image" => "images/t_3.webp",
     "prix" => "219 €",
@@ -32,7 +32,7 @@ $articleT_3 = [
 
 $articleT_4 = [
     "titre" => "Omegon Télescope Advanced 150/750 EQ-320",
-    "fabricant" => "Omegon"
+    "fabricant" => "Omegon",
     "description" => "Vous êtes passionnés de l'espace et souhaitez élargir vos horizons ? Vous voulez enfin voir étoiles, planètes, et nébuleuses et admirer la profondeur de l'univers ? C'est maintenant possible et pas besoin d'être un expert pour cela. Le télescope Omegon Advanced 150/750 EQ-320 est un télescope adapté pour les débutants et très simple d'utilisation. Avec sa grande puissance lumineuse, vous pourrez voir des nébuleuses situées à de nombreuses années lumières. Votre voyage à travers l'univers peut maintenant commencer, depuis votre balcon ou votre jardin !",
     "image" => "images/t_4.webp",
     "prix" => "449 €",
@@ -42,7 +42,7 @@ $articleT_4 = [
 
 $articleT_5 = [
     "titre" => "ZoomTélescope Dobson Meade N 82/300 LightBridge Mini 82 DOB",
-    "fabricant" => "Meade"
+    "fabricant" => "Meade",
     "description" => "Un télescope compact lumineux pour les astronomes, facile à transporter et simple à utiliser. C'est un télescope de type Dobson. Ces télescopes sont très faciles à utiliser pour les débutants et collectent beaucoup de lumière à partir des objets célestes. Le télescope Dobson est entièrement assemblé. On le pose simplement sur une table et on commence immédiatement l'exploration de l'espace.",
     "image" => "images/t_5.webp",
     "prix" => "59 €",
@@ -52,7 +52,7 @@ $articleT_5 = [
 
 $articleT_6 = [
     "titre" => "Télescope Dobson Meade N 130/650 LightBridge Mini 130 DOB",
-    "fabricant" => "Meade"
+    "fabricant" => "Meade",
     "description" => "Un télescope compact lumineux pour les astronomes, facile à transporter et simple à utiliser. C'est un télescope de type Dobson. Ces télescopes sont très faciles à utiliser pour les débutants et collectent beaucoup de lumière à partir des objets célestes. Le télescope Dobson est entièrement assemblé. On le pose simplement sur une table et on commence immédiatement l'exploration de l'espace.",
     "image" => "images/t_6.webp",
     "prix" => "229 €",
@@ -62,7 +62,7 @@ $articleT_6 = [
 
 $articleT_7 = [
     "titre" => "Télescope Skywatcher N 200/1000 Explorer BD NEQ-5",
-    "fabricant" => "Skywatcher"
+    "fabricant" => "Skywatcher",
     "description" => "Une ouverture de 200 mm révèle à l'observateur du ciel profond bien plus de détails que des télescopes plus petits. Grâce à un pouvoir collecteur supérieur de 78% à celui d'un télescope de 150 mm, on peut même distinguer les bras spiralés les plus fins de nombreuses galaxies et les structures d'autres nébuleuses. Les amas globulaires aussi, comme le fameux amas M13 dans Hercule, peuvent déjà être bien résolus sur leur bord. L'effet de courbure étant limité, le miroir primaire parabolique reproduit les objets avec une grande netteté. Le système optique donne d'excellents contrastes qui sont maintenus à un niveau optimal grâce à un miroir secondaire de seulement 52 mm et de fines branches de fixation. Tout cela permet des observations planétaires tout aussi nettes et lumineuses, même avec un plus fort grossissement. Le tube est équipé d'un solide porte-oculaire Crayford 50,8 mm permettant une mise au point sans décalage des images, ce qui a son importance notamment avec de forts grossissements et en astrophotographie. L'emploi d'oculaires de 50,8 mm permet de tirer profit des champs visuels les plus grands possibles. Le rapport d'ouverture de l'objectif est de f/5. Ce télescope fait donc partie des Newtons rapides. Ce type d'optique convient particulièrement bien pour la photographie. Le chercheur 9x50 est lui aussi considéré comme un atout, car sa grande ouverture permet de voir déjà des objets que l'on ne distingue pas à l'œil nu. Même son aspect extérieur fait de cet instrument un vrai bijou, avec ses lignes élégantes, son tube et son chercheur reluisant dans une teinte noire très esthétique.",
     "image" => "images/t_7.webp",
     "prix" => "749 €",
